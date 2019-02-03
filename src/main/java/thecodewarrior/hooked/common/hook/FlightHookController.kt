f740d9da83157926f58ba1f2d90089cc8599f7dc
Pierce Corcoran <pierce@plasticcow.com>
2019-02-03 15:51:02 -0800

    Added collinearity check for line constraint

--- src/main/java/thecodewarrior/hooked/common/hook/FlightHookController.kt
+++ src/main/java/thecodewarrior/hooked/common/hook/FlightHookController.kt
@@ -1,11 +1,14 @@
 package thecodewarrior.hooked.common.hook
 
+import com.teamwizardry.librarianlib.features.kotlin.div
+import com.teamwizardry.librarianlib.features.kotlin.dot
 import com.teamwizardry.librarianlib.features.kotlin.minus
 import com.teamwizardry.librarianlib.features.kotlin.plus
-import net.minecraft.entity.MoverType
+import com.teamwizardry.librarianlib.features.kotlin.times
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.math.Vec3d
 import thecodewarrior.hooked.common.util.clampLength
+import thecodewarrior.hooked.common.util.collinear
 
 class FlightHookController(
     /**
@@ -35,7 +38,7 @@ class FlightHookController(
 ): BasicHookController(type, player, fullCount, range, speed, 0.0, hookLength) {
     var jumpTimer = 0
     override fun moveBy(offset: Vec3d) {
-        if(plantedHooks.size < 3) return
+        if(plantedHooks.size <= 1) return
 //        player.setPosition(player.posX + offset.x, player.posY + offset.y, player.posZ + offset.z)
         val newOffset = constrainPos(getWaistPos(player), offset)
         player.motionX = newOffset.x
@@ -53,11 +56,32 @@ class FlightHookController(
     fun constrainPos(waist: Vec3d, offset: Vec3d): Vec3d {
         var pos = waist + offset
 
+        val points = plantedHooks.map { it.pos }
+        val collinear = points.collinear(0.50)
+        if(collinear != null) pos = constrainLine(pos, collinear.first, collinear.second)
+
         plantedHooks.forEach { hook ->
             pos = hook.pos + (pos - hook.pos).clampLength(this.range-0.5)
         }
 
         return (pos - waist).clampLength(1.0)
+    }
+
+    fun constrainLine(pos: Vec3d, a: Vec3d, b: Vec3d): Vec3d {
+        val aToB = (b - a)
+        val aToBLen = aToB.length()
+        val aToBNorm = aToB / aToBLen
+        val aToPos = pos - a
+
+        val dot = aToPos dot aToBNorm
+        if(dot < 0) return a
+        if(dot > aToBLen) return b
+
+        return a + aToBNorm * dot
+    }
+
+    fun constrainTri(pos: Vec3d): Vec3d {
+        return pos
     }
 
     override fun tick() {
--- src/main/java/thecodewarrior/hooked/common/util/Utils.kt
+++ src/main/java/thecodewarrior/hooked/common/util/Utils.kt
@@ -1,12 +1,17 @@
 package thecodewarrior.hooked.common.util
 
 import com.teamwizardry.librarianlib.features.kotlin.div
+import com.teamwizardry.librarianlib.features.kotlin.dot
+import com.teamwizardry.librarianlib.features.kotlin.minus
+import com.teamwizardry.librarianlib.features.kotlin.plus
 import com.teamwizardry.librarianlib.features.kotlin.times
 import net.minecraft.client.Minecraft
 import net.minecraft.util.math.MathHelper
 import net.minecraft.util.math.Vec3d
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sqrt
 
 fun Double.finiteOrDefault(defaultValue: Double) = if(this.isFinite()) this else defaultValue
@@ -28,4 +33,36 @@ fun Vec3d.clampLength(max: Double): Vec3d {
     if(lengthSquared < max * max) return this
     val length = sqrt(lengthSquared)
     return this * (max / length)
+}
+
+fun List<Vec3d>.collinear(threshold: Double): Pair<Vec3d, Vec3d>? {
+    val points = this.toMutableSet()
+    if(points.size < 2) return null
+
+    var origin = points.first()
+    points.remove(origin)
+    var endpoint = points.last()
+    points.remove(endpoint)
+    if(points.isEmpty()) return origin to endpoint
+
+    var axis = (endpoint - origin).normalize()
+
+    var max = (endpoint - origin).length()
+
+    val thresholdSq = threshold * threshold
+    points.forEach {
+        val dot = (it - origin) dot axis
+        if((axis * dot - (it - origin)).lengthSquared() > thresholdSq) return null
+        if(dot < 0) {
+            max += -dot
+            origin = it
+            axis = (endpoint - origin).normalize()
+        } else if(dot > max) {
+            max = dot
+            endpoint = it
+            axis = (endpoint - origin).normalize()
+        }
+    }
+
+    return origin to endpoint
 }
