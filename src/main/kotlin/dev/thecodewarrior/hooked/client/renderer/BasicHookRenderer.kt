bccfe43ff425c8a7f08ffb52f1d2175099e6b432
Pierce Corcoran <code@thecodewarrior.dev>
2020-11-15 16:30:10 -0800

    Apply lighting to hooks

--- src/main/kotlin/dev/thecodewarrior/hooked/client/renderer/BasicHookRenderer.kt
+++ src/main/kotlin/dev/thecodewarrior/hooked/client/renderer/BasicHookRenderer.kt
@@ -5,6 +5,7 @@ import com.teamwizardry.librarianlib.core.bridge.IRenderTypeState
 import com.teamwizardry.librarianlib.core.util.*
 import com.teamwizardry.librarianlib.core.util.kotlin.color
 import com.teamwizardry.librarianlib.core.util.kotlin.loc
+import com.teamwizardry.librarianlib.core.util.kotlin.normal
 import com.teamwizardry.librarianlib.core.util.kotlin.pos
 import com.teamwizardry.librarianlib.math.*
 import de.javagl.obj.*
@@ -15,11 +16,15 @@ import dev.thecodewarrior.hooked.util.getWaistPos
 import net.minecraft.client.renderer.IRenderTypeBuffer
 import net.minecraft.client.renderer.RenderState
 import net.minecraft.client.renderer.RenderType
+import net.minecraft.client.renderer.WorldRenderer
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.profiler.IProfiler
 import net.minecraft.resources.IResourceManager
 import net.minecraft.util.ResourceLocation
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.World
 import org.lwjgl.opengl.GL11
 import java.awt.Color
 import java.io.IOException
@@ -27,13 +32,12 @@ import java.io.IOException
 class BasicHookRenderer(val id: ResourceLocation):
     HookRenderer<BasicHookPlayerController>(), ISimpleReloadListener<BasicHookRenderer.ReloadData> {
 
-    var model: Obj = Objs.create()
-    var modelVertexIndices: IntArray = IntArray(0)
-    val modelLocation = loc(id.namespace, "models/hook/${id.path}.obj")
-    val hookRenderType =
-        SimpleRenderTypes.flat(loc(id.namespace, "textures/hook/${id.path}/hook.png"), GL11.GL_TRIANGLES)
-    val chain1RenderType = createChainType(loc(id.namespace, "textures/hook/${id.path}/chain1.png"))
-    val chain2RenderType = createChainType(loc(id.namespace, "textures/hook/${id.path}/chain2.png"))
+    private var model: Obj = Objs.create()
+    private var modelVertexIndices: IntArray = IntArray(0)
+    private val modelLocation = loc(id.namespace, "models/hook/${id.path}.obj")
+    private val hookRenderType = createHookRenderType(loc(id.namespace, "textures/hook/${id.path}/hook.png"))
+    private val chain1RenderType = createChainRenderType(loc(id.namespace, "textures/hook/${id.path}/chain1.png"))
+    private val chain2RenderType = createChainRenderType(loc(id.namespace, "textures/hook/${id.path}/chain2.png"))
 
     init {
         Client.resourceReloadHandler.register(this)
@@ -59,13 +63,14 @@ class BasicHookRenderer(val id: ResourceLocation):
         val waist = player.getWaistPos(partialTicks)
         val hookPos = hook.posLastTick + (hook.pos - hook.posLastTick) * partialTicks
         val chainLength = waist.distanceTo(hookPos)
+        val chainDirection = (hookPos - waist) / chainLength
 
         matrix.push()
         matrix.translate(waist)
         matrix.rotate(Quaternion.fromRotationTo(vec(0, 1, 0), hookPos - waist))
 
-        drawHalfChain(matrix, chain1RenderType, chainLength, 0.5, 0.0)
-        drawHalfChain(matrix, chain2RenderType, chainLength, 0.0, 0.5)
+        drawHalfChain(matrix, chain1RenderType, player.world, waist, chainDirection, chainLength, 0.5, 0.0)
+        drawHalfChain(matrix, chain2RenderType, player.world, waist, chainDirection, chainLength, 0.0, 0.5)
 
         matrix.pop()
 
@@ -76,11 +81,18 @@ class BasicHookRenderer(val id: ResourceLocation):
 
         val buffer = IRenderTypeBuffer.getImpl(Client.tessellator.buffer)
         val vb = buffer.getBuffer(hookRenderType)
+        val lightmap = getBrightnessForRender(player.world, BlockPos(hookPos))
         modelVertexIndices.forEach { index ->
             val vertex = model.getVertex(index)
             val tex = model.getTexCoord(index)
-//            val normal = model.getNormal(index)
-            vb.pos(matrix, vertex.x, vertex.y, vertex.z).color(Color.WHITE).tex(tex.x, 1-tex.y).endVertex()
+            val normal = model.getNormal(index)
+            vb.pos(matrix, vertex.x, vertex.y, vertex.z).color(Color.WHITE).tex(tex.x, 1 - tex.y)
+                .overlay(0, 0)
+                .lightmap(lightmap)
+                .normal(
+                    matrix.transformDelta(vec(normal.x, normal.y, normal.z)).normalize()
+                )
+                .endVertex()
         }
         buffer.finish()
 
@@ -90,51 +102,112 @@ class BasicHookRenderer(val id: ResourceLocation):
     /**
      * Draws half of a chain. The deltaX and deltaZ parameters dictate whether to draw the ±X or ±Z part of the chain.
      */
-    fun drawHalfChain(matrix: Matrix4d, renderType: RenderType, chainLength: Double, deltaX: Double, deltaZ: Double) {
+    fun drawHalfChain(
+        matrix: Matrix4d,
+        renderType: RenderType,
+        world: World,
+        waist: Vec3d,
+        chainDirection: Vec3d,
+        chainLength: Double,
+        deltaX: Double,
+        deltaZ: Double
+    ) {
         val chainSegments = floorInt(chainLength)
         val firstSegmentLength = chainLength - chainSegments
 
         val buffer = IRenderTypeBuffer.getImpl(Client.tessellator.buffer)
 
+        val normal = matrix.transformDelta(vec(deltaZ, 0, deltaX)).normalize()
+        val rnormal = -normal
+
         val vb = buffer.getBuffer(renderType)
         if (firstSegmentLength > chainLengthEpsilon) {
+            val lightPos = BlockPos(waist + chainDirection * (firstSegmentLength / 2))
+            val lightmap = getBrightnessForRender(world, lightPos)
+
             val minV = 1 - firstSegmentLength.toFloat()
-            vb.pos(matrix, -deltaX, 0, -deltaZ).color(Color.WHITE).tex(0f, minV).endVertex()
-            vb.pos(matrix, deltaX, 0, deltaZ).color(Color.WHITE).tex(1f, minV).endVertex()
-            vb.pos(matrix, deltaX, firstSegmentLength, deltaZ).color(Color.WHITE).tex(1f, 1f).endVertex()
-            vb.pos(matrix, -deltaX, firstSegmentLength, -deltaZ).color(Color.WHITE).tex(0f, 1f).endVertex()
+            val len = firstSegmentLength
+
+            vb.pos(matrix, -deltaX, 0, -deltaZ).color(Color.WHITE).tex(0f, minV)
+                .overlay(0, 0).lightmap(lightmap).normal(normal).endVertex()
+            vb.pos(matrix, deltaX, 0, deltaZ).color(Color.WHITE).tex(1f, minV)
+                .overlay(0, 0).lightmap(lightmap).normal(normal).endVertex()
+            vb.pos(matrix, deltaX, len, deltaZ).color(Color.WHITE).tex(1f, 1f)
+                .overlay(0, 0).lightmap(lightmap).normal(normal).endVertex()
+            vb.pos(matrix, -deltaX, len, -deltaZ).color(Color.WHITE).tex(0f, 1f)
+                .overlay(0, 0).lightmap(lightmap).normal(normal).endVertex()
+
+            vb.pos(matrix, -deltaX, len, -deltaZ).color(Color.WHITE).tex(0f, 1f)
+                .overlay(0, 0).lightmap(lightmap).normal(rnormal).endVertex()
+            vb.pos(matrix, deltaX, len, deltaZ).color(Color.WHITE).tex(1f, 1f)
+                .overlay(0, 0).lightmap(lightmap).normal(rnormal).endVertex()
+            vb.pos(matrix, deltaX, 0, deltaZ).color(Color.WHITE).tex(1f, minV)
+                .overlay(0, 0).lightmap(lightmap).normal(rnormal).endVertex()
+            vb.pos(matrix, -deltaX, 0, -deltaZ).color(Color.WHITE).tex(0f, minV)
+                .overlay(0, 0).lightmap(lightmap).normal(rnormal).endVertex()
         }
         for (i in 0 until chainSegments) {
             val yPos = firstSegmentLength + i
 
-            vb.pos(matrix, -deltaX, yPos, -deltaZ).color(Color.WHITE).tex(0f, 0f).endVertex()
-            vb.pos(matrix, deltaX, yPos, deltaZ).color(Color.WHITE).tex(1f, 0f).endVertex()
-            vb.pos(matrix, deltaX, yPos + 1, deltaZ).color(Color.WHITE).tex(1f, 1f).endVertex()
-            vb.pos(matrix, -deltaX, yPos + 1, -deltaZ).color(Color.WHITE).tex(0f, 1f).endVertex()
+            val lightPos = BlockPos(waist + chainDirection * (yPos + 0.5))
+            val lightmap = getBrightnessForRender(world, lightPos)
+
+            vb.pos(matrix, -deltaX, yPos, -deltaZ).color(Color.WHITE).tex(0f, 0f)
+                .overlay(0, 0).lightmap(lightmap).normal(normal).endVertex()
+            vb.pos(matrix, deltaX, yPos, deltaZ).color(Color.WHITE).tex(1f, 0f)
+                .overlay(0, 0).lightmap(lightmap).normal(normal).endVertex()
+            vb.pos(matrix, deltaX, yPos + 1, deltaZ).color(Color.WHITE).tex(1f, 1f)
+                .overlay(0, 0).lightmap(lightmap).normal(normal).endVertex()
+            vb.pos(matrix, -deltaX, yPos + 1, -deltaZ).color(Color.WHITE).tex(0f, 1f)
+                .overlay(0, 0).lightmap(lightmap).normal(normal).endVertex()
+
+            vb.pos(matrix, -deltaX, yPos + 1, -deltaZ).color(Color.WHITE).tex(0f, 1f)
+                .overlay(0, 0).lightmap(lightmap).normal(rnormal).endVertex()
+            vb.pos(matrix, deltaX, yPos + 1, deltaZ).color(Color.WHITE).tex(1f, 1f)
+                .overlay(0, 0).lightmap(lightmap).normal(rnormal).endVertex()
+            vb.pos(matrix, deltaX, yPos, deltaZ).color(Color.WHITE).tex(1f, 0f)
+                .overlay(0, 0).lightmap(lightmap).normal(rnormal).endVertex()
+            vb.pos(matrix, -deltaX, yPos, -deltaZ).color(Color.WHITE).tex(0f, 0f)
+                .overlay(0, 0).lightmap(lightmap).normal(rnormal).endVertex()
         }
         buffer.finish()
     }
 
-    private fun createChainType(texture: ResourceLocation): RenderType {
+    private fun createHookRenderType(texture: ResourceLocation): RenderType {
         val stateBuilder = RenderType.State.getBuilder()
             .texture(RenderState.TextureState(texture, false, false))
             .alpha(DefaultRenderStates.DEFAULT_ALPHA)
             .depthTest(DefaultRenderStates.DEPTH_LEQUAL)
             .transparency(DefaultRenderStates.TRANSLUCENT_TRANSPARENCY)
             .cull(DefaultRenderStates.CULL_DISABLED)
+            .lightmap(DefaultRenderStates.LIGHTMAP_ENABLED)
+            .diffuseLighting(DefaultRenderStates.DIFFUSE_LIGHTING_ENABLED)
         val state = stateBuilder.build(true)
 
-        // CULL_DISABLED doesn't actually work, so we have to do it ourself
-        @Suppress("CAST_NEVER_SUCCEEDS")
-        (state as IRenderTypeState).addState("working_cull", {
-            RenderSystem.disableCull()
-        }, {
-            RenderSystem.enableCull()
-        })
-
-        return SimpleRenderTypes.makeType("flat_texture",
-            DefaultVertexFormats.POSITION_COLOR_TEX, GL11.GL_QUADS, 256, false, false, state
+        return SimpleRenderTypes.makeType(
+            "hooked_hook",
+            DefaultVertexFormats.ENTITY, GL11.GL_TRIANGLES, 256, false, false, state
         )
+    }
+
+    private fun createChainRenderType(texture: ResourceLocation): RenderType {
+        val stateBuilder = RenderType.State.getBuilder()
+            .texture(RenderState.TextureState(texture, false, false))
+            .alpha(DefaultRenderStates.DEFAULT_ALPHA)
+            .depthTest(DefaultRenderStates.DEPTH_LEQUAL)
+            .transparency(DefaultRenderStates.TRANSLUCENT_TRANSPARENCY)
+            .lightmap(DefaultRenderStates.LIGHTMAP_ENABLED)
+            .diffuseLighting(DefaultRenderStates.DIFFUSE_LIGHTING_ENABLED)
+        val state = stateBuilder.build(true)
+
+        return SimpleRenderTypes.makeType(
+            "hooked_chain",
+            DefaultVertexFormats.ENTITY, GL11.GL_QUADS, 256, false, false, state
+        )
+    }
+
+    private fun getBrightnessForRender(world: World, pos: BlockPos): Int {
+        return if (world.isBlockLoaded(pos)) WorldRenderer.getCombinedLight(world, pos) else 0
     }
 
     data class ReloadData(val model: Obj)
